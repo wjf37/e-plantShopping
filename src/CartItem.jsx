e6eff45b94ce84f2299ac82366896ebcf2f61fa1
wjf37 <vincentw3007@gmail.com>
2025-06-15 11:15:47 -0400

    task 3 done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const total = 0;
+    cart.forEach(item => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    dispatch(onContinueShopping(e));
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference");
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    } else if (item.quantity === 1){
+        dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const total = item.quantity * parseFloat(item.cost.substring(1));
   };
 
   return (
